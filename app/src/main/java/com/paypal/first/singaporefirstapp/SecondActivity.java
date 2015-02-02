f7b4f1a87bd9c1be709ed339f86e2a8db68c2205
Lange Pantoja <wpantoja@paypal.com>
2015-02-02 15:57:44 +0800

    Adding implicit intent on second button to call up a number

--- app/src/main/java/com/paypal/first/singaporefirstapp/SecondActivity.java
+++ app/src/main/java/com/paypal/first/singaporefirstapp/SecondActivity.java
@@ -1,9 +1,13 @@
 package com.paypal.first.singaporefirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class SecondActivity extends ActionBarActivity {
@@ -12,6 +16,14 @@ public class SecondActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void doSecondButtonClick(View v) {
+        Log.i(MainActivity.TAG, "Second Button is clicked");
+        // Implicit Intent
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:97365777"));
+        startActivity(intent);
     }
 
 
